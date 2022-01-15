ff8b8eb8a68bf087ea1d01ac16ed4c3ad6dfbfca
james rowland <rowlanja@tcd.ie>
2022-01-15 19:42:50 +0000

    finished portfolioTracker1.0

--- tracker.py
+++ tracker.py
@@ -1,20 +1,101 @@
 from datetime import datetime, date
-from coinbase.wallet.client import Client
-import csv  
+import os
+import csv
+from binance.client import Client
+  
+class Position:
+    def __init__(self, ticker, entry, size, exit, lvs, margin):
+        self.ticker = ticker
+        self.entry = entry
+        self.size = size
+        self.exit = exit
+        self.lvs = lvs
+        self.margin = margin
+        self.gain = '--:-- %'
+        self.profit = '$ ---.---'
+        self.currentPrice = '$ ---.---'
+        self.dateTime = '--/--/--'
 
+    def getTicker(self):
+        return self.ticker
+    def setTicker(self, x):
+        self.ticker = x
 
+    def getEntry(self):
+        return self.entry
+    def setEntry(self, x):
+        self.entry = x
 
-header = ['ticker', 'entry', 'currentPrice','sl','tp1', 'tp2', 'tp3', '% Gain', 'Profit', 'Datetime']
-positions = []
-coinbase_API_key = ""
-coinbase_API_secret = ""
+    def getSize(self):
+        return self.size
+    def setSize(self, x):
+        self.size = x
+
+    def getExit(self):
+        return self.exit
+    def setExit(self, x):
+        self.exit = x
+
+    def getLVS(self):
+        return self.lvs
+    def setLVS(self, x):
+        self.lvs = x
+
+    def getGain(self):
+        return self.gain
+    def setGain(self, x):
+        self.gain = x
+
+    def getProfit(self):
+        return self.profit
+    def setProfit(self, x):
+        self.profit = x
+
+    def getCurrentPrice(self):
+        return self.currentPrice
+    def setCurrentPrice(self, x):
+        self.currentPrice = x
+
+    def getMargin(self):
+        return self.margin
+    def setCurrentMargin(self, x):
+        self.margin = x
+
+    def getDatetime(self):
+        return self.dateTime
+    def setDatetime(self, x):
+        self.dateTime = x
+
+    def toArr(self):
+        return [
+            self.ticker,
+            self.entry,
+            self.size,
+            self.margin,
+            self.exit,
+            self.gain,
+            self.profit,
+            self.dateTime,
+            self.ticker,
+            self.lvs
+        ]
+    
+header = ['Ticker', 'Entry','Size (ticker)','Margin (usd)','Exit', '% Gain', 'Profit', 'Datetime', 'Long or Short']
+newPositions = []
+api_key = os.environ.get('binance_api')
+api_secret = os.environ.get('binance_secret')
 
 def updateCurrentPrice(position):
-    ticker = position[0]
-    date = date.today()
-    client = Client(coinbase_API_key, coinbase_API_secret)
-    price = client.get_spot_price(currency_pair= '-USD', date=date)  
-    print(price)
+    ticker = position.getTicker()
+    client = Client(api_key, api_secret)
+    currentPrice = client.get_symbol_ticker(symbol=ticker+"USDT")
+    dollarDifference = (position.getSize() * float(currentPrice["price"])) - (position.getSize() * float(position.getEntry()))
+    position.setProfit(dollarDifference)
+    position.setGain(str(round(dollarDifference/position.getMargin(),5)*100) + "%")
+    position.setDatetime(datetime.now())
+    print(currentPrice["price"])
+    print('$ gain', position.getGain())
+    print('$ profit', position.getProfit())
     return position
 
 def createPositions():
@@ -22,16 +103,14 @@ def createPositions():
     while(not finished ):
         print("ADDING POSITION TO DATABASE")
         ticker = input("Ticker : ").upper()
-        entry = input("Entry : ")
-        sl = input("SL : ")
-        tp1 = input("TP1 : ")
-        tp2 = input("TP2 : ")
-        tp3 = input("TP3 : ")
-        time = datetime.now()
-        gain = '--:-- %'
-        profit = '$ ---.---'
-        currentPrice = '$ ---.---'
-        positions.add([ticker, entry, currentPrice, sl, tp1, tp2, tp3, gain, profit, time])
+        entry = float(input("Entry : "))
+        size = float(input("Size (ticker) : "))
+        margin = int(input("Margin : "))
+        exit = float(input("Exit : "))
+        lvs = input("Long or Short?(l/s) : ")
+        position = Position(ticker, entry, size, exit, lvs, margin)
+        updateCurrentPrice(position)
+        newPositions.append(position)
         if(input("Are you finished ? (y/n) : ") == 'y') : finished = True
 
 
@@ -40,8 +119,9 @@ def writePositions():
         writer = csv.writer(f)
         writer.writerow(header)
 
-        for entry in positions : 
-            writer.writerow(entry)  
+        for position in newPositions : 
+            position = position.toArr()    
+            writer.writerow(position)  
 
 createPositions()
 writePositions()
